059b01ccf517f70257a9745e95ac37163ba4a46b
Olga Borzenkova <kyssike@gmail.com>
2016-10-05 13:21:31 +0300

    Panacea - Truth Or Lie solved

---
+++ Easy/Panacea_TruthOrLie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+	static void Main (string[] args)
+	{
+		using (StreamReader reader = File.OpenText (args [0]))
+			while (!reader.EndOfStream) {
+				string line = reader.ReadLine ();
+				if (null == line)
+					continue;
+				var input = line.Split ('|');
+
+				var virus = Array.ConvertAll (input [0].Trim ().Split (' '), x => Convert.ToInt32 (x, 16));
+				var antivirus = Array.ConvertAll (input [1].Trim ().Split (' '), x => Convert.ToInt32 (x, 2));
+				Console.WriteLine (virus.Sum () <= antivirus.Sum ());
+			}
+	}
+}
+
+
+
+
