174507fa643bec7ae6a007bc2dacede9caa1001f
Olga Borzenkova <kyssike@gmail.com>
2016-10-06 14:19:49 +0300

    String Mask solved

---
+++ Easy/RacingChars.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var prevIndex = -1;
+
+		using (StreamReader reader = File.OpenText(args[0]))
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+					continue;
+				// do something with line
+
+				var index = line.IndexOf('C');
+				char current;
+				if (index != -1) {
+					current = 'C';
+				} else {
+					index = line.IndexOf('_');
+					current = '_';
+				}
+				char direction;
+				if (prevIndex == -1 || prevIndex == index) {
+					direction = '|';
+				} else {
+					direction = (prevIndex > index) ? '/' : '\\';
+				}
+				prevIndex = index;
+				var result = line.Replace(current, direction);
+
+				Console.WriteLine (result);
+			}
+	}
+}
+
+
---
+++ Easy/StringMask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (StreamReader reader = File.OpenText(args[0]))
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+					continue;
+				// do something with line
+
+				var arr = line.Split (' ');
+				var word = arr [0].ToCharArray();
+				var binary = arr [1];
+
+				for (int i = 0; i < word.Length; i++) {
+					if (binary [i] == '1') {
+						word [i] = char.ToUpper (word [i]);
+					}
+				}
+				var result = string.Join("", word);
+
+				Console.WriteLine (result);
+			}
+	}
+}
+
+
+
