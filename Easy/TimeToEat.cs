3b0ac1dc40ef4c889801c1b66cdd668a5340742d
Olga Borzenkova <kyssike@gmail.com>
2016-10-10 14:16:19 +0300

    Time To Eat solved

---
+++ Easy/TimeToEat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (StreamReader reader = File.OpenText(args[0]))
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+					continue;
+				// do something with line
+
+				var timestamps = Array.ConvertAll(line.Split (' '), TimeSpan.Parse);
+
+				Array.Sort (timestamps, delegate(TimeSpan x, TimeSpan y) {
+					return y.CompareTo(x);
+				});
+
+				var result = string.Join (" ", timestamps);
+				Console.WriteLine (result);
+			}
+	}
+}
+
+
