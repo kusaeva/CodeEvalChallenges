551d74c10c9ded3b00678d0888b6741d4bbf9f9b
Olga Borzenkova <kyssike@gmail.com>
2016-10-11 13:44:38 +0300

    Max Range Sum solved

---
+++ Easy/MaxRangeSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (StreamReader reader = File.OpenText(args[0]))
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+					continue;
+				// do something with line
+				var input = line.Split (';');
+				var daysCount = Int32.Parse (input [0]);
+				var gains = Array.ConvertAll (input [1].Split (' '), Int32.Parse);
+
+				var maxGain = 0;
+				for (var i = 0; i < gains.Length - daysCount + 1; i++) {
+					var gain = 0;
+					for (var j = 0; j < daysCount; j++) {
+						gain += gains [i + j];
+					}
+
+					maxGain = gain > maxGain ? gain : maxGain;
+				}
+
+				var result = maxGain.ToString ();
+				Console.WriteLine (result);
+			}
+	}
+}
