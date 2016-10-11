5a1bf2b953e0e96a1e3d7e6d49be326200127e08
Olga Borzenkova <kyssike@gmail.com>
2016-10-11 10:46:11 +0300

    Roller Coaster solved

---
+++ Easy/RollerCoaster.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (StreamReader reader = File.OpenText(args[0]))
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				if (null == line)
+					continue;
+				// do something with line
+				var isUpper = true;
+				var charArray = line.ToCharArray ();
+				for (int i = 0; i < charArray.Length; i++) {
+					var ch = charArray[i];
+					if (char.IsLetter (ch)) {
+						charArray[i] = isUpper ? char.ToUpper (ch) : char.ToLower(ch);
+						isUpper = !isUpper;
+					}
+				}
+				Console.WriteLine (string.Join("",charArray));
+			}
+	}
+}
